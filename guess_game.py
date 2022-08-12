c11dac16336d64a300b959fb88178e1d348f68e0
eribertto_fed36 <erimendz@gmail.com>
2022-08-12 18:58:36 +0300

    add note max num value to 100

--- guess_game.py
+++ guess_game.py
@@ -10,7 +10,7 @@ import random
 
 def guessing_game():
     # method randint begins 0-99 excluding 100
-    answer = random.randint(0, 100)
+    answer = random.randint(1, 101)
     while True:
         #user_guess = int(input('What is your guess? (numbers only) '))
         user_guess = (input('What is your guess? (numbers only) '))
@@ -18,6 +18,9 @@ def guessing_game():
             print("Only digit numbers are allowed. Try again!")
             break
         user_guess = int(user_guess)
+        if user_guess > 100:
+            print('Max number allowed is up to 100 only')
+            break
         if user_guess == answer:
             print(f'Right! The answer is {user_guess}')
             break
