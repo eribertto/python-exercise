751f3db6ae17d27b88bbae0b1b7898c509863dd4
eribertto_fed36 <erimendz@gmail.com>
2022-08-12 19:44:18 +0300

    add loop controls

---
+++ loop_controls.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+
+# Bro Code Python course
+# https://www.youtube.com/watch?v=XKHEtdqhLK8
+# all about loop controls in Python
+
+# break - used to terminate the loop entirely
+# continue - skips to the next iteration of the loop
+# pass - does nothing, acts like a placeholder
+
+# break
+#while True:
+#    name = input("Enter your name: ")
+#    if name != "":
+#        break
+#print(f'Hello there {name}')
+#
+## continue
+#phone_number = "123-456-789"
+#print(f'Old number is {phone_number}')
+#for i in phone_number:
+#    # skip the dash symbol
+#    if i == "-":
+#        continue
+#    print(i, end="")
+
+# pass 
+for i in range(1, 21):
+    # skip unlucky number 13
+    if i == 13:
+        pass
+    else:
+        print(i)
