974f222e1da513b6517b3a2025d2b576f11ee6fb
eribertto_fed36 <erimendz@gmail.com>
2022-08-12 18:48:36 +0300

    add input validation

--- guess_game.py
+++ guess_game.py
@@ -12,7 +12,12 @@ def guessing_game():
     # method randint begins 0-99 excluding 100
     answer = random.randint(0, 100)
     while True:
-        user_guess = int(input('What is your guess? (numbers only) '))
+        #user_guess = int(input('What is your guess? (numbers only) '))
+        user_guess = (input('What is your guess? (numbers only) '))
+        if not user_guess.isdigit():
+            print("Only digit numbers are allowed. Try again!")
+            break
+        user_guess = int(user_guess)
         if user_guess == answer:
             print(f'Right! The answer is {user_guess}')
             break
