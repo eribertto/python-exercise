5ee67be85c2e9260e3f17620e0fe8bef4dc1db92
eribertto_fed36 <erimendz@gmail.com>
2022-09-07 05:29:40 +0300

    reset to latest commit

---
+++ type_hints_sampler.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+# all about type hints in Python
+# https://fastapi.tiangolo.com/python-types/
+
+# type hints for string variables in the func definition
+def get_full_name(first_name: str, last_name: str):
+    full_name = first_name.title() + " " + last_name.title()
+    return full_name
+
+# call the function
+print('Hello there ', (get_full_name('james', 'bond 007')))
