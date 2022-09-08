cccc742c4f9e7374c43d7bb74ad590da7d2154d4
eribertto_fed36 <erimendz@gmail.com>
2022-09-08 05:49:17 +0300

    change web site to scrape and test run it

--- beautifulsoup_demo.py
+++ beautifulsoup_demo.py
@@ -20,36 +20,11 @@ import csv
 # Note this will not run on online IDEs
 
 # Step 4 Searching and navigating through the parse tree
-URL = "https://www.values.com/inspirational-quotes/"
+URL = "https://www.warayblogger.com/2012/05/waray-songs-on-life-and-love.html"
 r = requests.get(URL)
 soup = BeautifulSoup(r.content, "html5lib")
-# print(soup.prettify())		# this prints the parse tree e.g. the complete html page
-quotes = []  # a list to store the quotes
-table = soup.find("div", attrs={"id": "all_quotes"})
-for row in table.findAll(
-    "div",
-    attrs={"class": "col-6 col-lg-3 text-center margin-30px-bottom sm-margin-30px-top"},
-):
-    quote = {}
-    quote["theme"] = row.h5.txt
-    quote["url"] = row.a["href"]
-    quote["img"] = row.img["src"]
-    quote["lines"] = row.img["alt"].split(" #")[0]
-    quote["author"] = row.img["alt"].split(" #")[1]
-    quotes.append(quote)
-
-filename = "inspirational_quotes.csv"
-with open(filename, "w", newline="") as f:
-    w = csv.DictReader(f, ["theme", "url", "img", "lines", "author"])
-    w.writeheader()
-    for quote in quotes:
-        w.writerow(quote)
-
-"""
-error when code is run:
-File "/home/eribertto/online-repos/web-scraper/beautifulsoup_demo.py", line 42, in <module>
-    w.writeheader()
-AttributeError: 'DictReader' object has no attribute 'writeheader'
-possible solution:
-https://bobbyhadz.com/blog/python-attributeerror-dict-object-has-no-attribute
-"""
+# check the object type and save as variable
+parse_tree = soup.prettify()
+print(parse_tree)		# this prints the parse tree e.g. the complete html page
+# how to save this file locally?
+print(type(soup))
