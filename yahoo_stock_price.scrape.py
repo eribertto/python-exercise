82175b1ffb71616c17cc0c71de13de57e23494ac
eribertto_fed36 <erimendz@gmail.com>
2022-09-13 22:32:52 +0300

    do refactoring by sourcery

--- yahoo_stock_price.scrape.py
+++ yahoo_stock_price.scrape.py
@@ -27,13 +27,8 @@ def computequoteprice():
     url_requests = requests.get(url_of_page, headers=headers)
     print(url_requests)
     soup_ocreate = bs(url_requests.text, "lxml")
-    #quote_price = soup_ocreate.find(
-    #    "span", class_="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"
-    #).text
-    # new code using the method find
-    quote_price = soup_ocreate.find("fin-streamer", attrs={"data-symbol": SYMBOL, "data-field": "regularMarketPrice"},).get_text()
-    return quote_price
-print("Quote price = " + str(computequoteprice()))
+    return soup_ocreate.find("fin-streamer", attrs={"data-symbol": SYMBOL, "data-field": "regularMarketPrice"},).get_text()
+print(f"Quote price = {str(computequoteprice())}")
 """
 get_stock_prices_beautifulsoup.py", line 14, in computequoteprice
     quote_price = soup_ocreate.find('span', class_='Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)').text
