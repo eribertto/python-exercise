70b39e00e7fde56a854d6d66fedc8d56accf115e
eribertto_fed36 <erimendz@gmail.com>
2022-09-13 22:01:08 +0300

    add solution codes but still gets error NoneType

--- yahoo_stock_price.scrape.py
+++ yahoo_stock_price.scrape.py
@@ -4,18 +4,31 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-url_of_page = "https://finance.yahoo.com/quote/%5EIXIC/"
+# add global constant variable
+SYMBOL = "EIXIX"
+url_of_page = f"https://finance.yahoo.com/quote/{SYMBOL}"
+#url_requests = requests.get(url_of_page)
+#print(url_requests)	# returns <Response [200]> meaning reachable
 
 
 def computequoteprice():
-    url_requests = requests.get(url_of_page)
+    # add a User-Agent - this comes down to experience with web scraping and can be useful
+    # https://www.shellhacks.com/python-requests-user-agent-web-scraping/
+    '''
+    For Vivaldi:
+    User Agent	Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.167 Safari/537.36
+    '''
+ 
+    headers = {'User Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.167 Safari/537.36'}
+    url_requests = requests.get(url_of_page, headers=headers)
+    print(url_requests)
     soup_ocreate = bs(url_requests.text, "lxml")
-    quote_price = soup_ocreate.find(
-        "span", class_="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"
-    ).text
+    #quote_price = soup_ocreate.find(
+    #    "span", class_="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"
+    #).text
+    # new code using the method find
+    quote_price = soup_ocreate.find("fin-streamer", attrs={"data-symbol": SYMBOL, "data-field": "regularMarketPrice"}).get_text()
     return quote_price
-
-
 print("Quote price = " + str(computequoteprice()))
 """
 get_stock_prices_beautifulsoup.py", line 14, in computequoteprice
