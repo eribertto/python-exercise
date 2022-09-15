0ceb750f2c0132ce1c73d9a99a3ec791fbb710b2
erimendz_fed36 <erimendz@gmail.com>
2022-09-15 18:07:03 +0300

    add formatting of comment

--- guess_game.py
+++ guess_game.py
@@ -12,7 +12,7 @@ def guessing_game():
     # method randint begins 0-99 excluding 100
     answer = random.randint(1, 101)
     while True:
-        #user_guess = int(input('What is your guess? (numbers only) '))
+        # user_guess = int(input('What is your guess? (numbers only) '))
         user_guess = (input('What is your guess? (numbers only) '))
         if not user_guess.isdigit():
             print("Only digit numbers are allowed. Try again!")
