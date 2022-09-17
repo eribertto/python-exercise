5f1e8d07a7c7b26cb0246a40efda0e69d2e5cea4
erimendz_fed36_cinammon <erimendz@gmail.com>
2022-09-17 20:36:03 +0300

    make list comprehension line

--- read_lines_exercise.py
+++ read_lines_exercise.py
@@ -3,23 +3,26 @@
 
 # reading lines of a file
 # page 187 of the book Python Crash Course 2ed PCC
+# NOTE: use type hints to have better code completion
 
 # define the target file
-filename = 'argparser.demo.py'
+filename = "argparser.demo.py"
 
 
-with open(filename) as file_object:
-    for line in file_object:
-        #print(line)     # prints line with double spaces
-        print(line.rstrip())     # strips the extra newlines
+#with open(filename) as f:
+#    for line in f:
+#        # print(line)     # prints line with double spaces
+#        print(line.rstrip())  # strips the extra newlines
 
 # making a list of lines
-with open(filename) as file_object:
-    lines = file_object.readlines()
-    print(type(lines))      # this is type list
+with open(filename) as f:
+    lines = f.readlines()
+    print(lines)  # this is type list
+    print("\nPrinting line by line:\n")
 
 for line in lines:
     print(line.rstrip())
 
-#NOTE: use type hints to have better code completion
-
+# get each line as a list
+content_list = [line.strip() for line in lines]
+print(content_list)
