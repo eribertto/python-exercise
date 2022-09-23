bef5932c4c9c89191c10f82f61678f40cbe5637b
erimendz_fed36_cinammon <erimendz@gmail.com>
2022-09-23 13:08:07 +0300

    coded the add function

--- write_to_notes_file.py
+++ write_to_notes_file.py
@@ -7,38 +7,40 @@
 filename: str = "/home/erimendz/note.txt"
 
 
+
+# add if name = main prelude
+## read the whole file first
+#with open(filename) as f:
+#    print(f.read().strip("\n"))
+
+
+# function add_todo
+def add_todo():
+    note_to_write = input("\nWhat todo task to add? ")
+    if len(note_to_write) == 0:
+        print("None given, try again next time :-) ")
+        exit()
+    # confirm to proceed
+    confirm_msg = "Proceed to write [N/y]?"
+    ask_confirm = input(confirm_msg)
+    # if ['y', 'ye', 'yes', 'YES'] in ask_confirm:    # lol this is written backwards
+    if ask_confirm in ["y", "ye", "yes", "YES"]:
+        # if yes, make upper case and write, else discard silently
+        note_to_write = note_to_write.upper()
+        with open(filename, "a") as f:
+            f.write("\n===||===\n")  # make this a function
+            f.write(note_to_write)
+    else:
+        print("Bye!")
+
 def get_choice():
     choice = input("[A]dd or [D]elete todo? ")
     if choice in ["a", "A"]:
-        print("Do add function!")
+        add_todo()
     elif choice in ["d", "D"]:
-        print(("Do delete function"))
+        print("Do delete function")
 
 get_choice()
-
-
-# read the whole file first
-with open(filename) as f:
-    print(f.read().strip("\n"))
-
-# ask user
-note_to_write = input("\nWhat todo task to add? ")
-if len(note_to_write) == 0:
-    print("None given, try again next time :-) ")
-    exit()
-# confirm to proceed
-confirm_msg = "Proceed to write [N/y]?"
-ask_confirm = input(confirm_msg)
-# if ['y', 'ye', 'yes', 'YES'] in ask_confirm:    # lol this is written backwards
-if ask_confirm in ["y", "ye", "yes", "YES"]:
-    # if yes, make upper case and write, else discard silently
-    note_to_write = note_to_write.upper()
-    with open(filename, "a") as f:
-        f.write("===||===\n")  # make this a function
-        f.write(note_to_write)
-else:
-    print("Bye!")
-
 """
 feature enhancements:
 1. make this executable to be available in user environment
@@ -48,6 +50,7 @@ feature enhancements:
     tip: use module re
 5. for add function use the above
 6. save working file in python-exercise repo
+7. add name = main prelude
 
 NB: for tabular format aka to use free space of desket
 consider using bash column command
