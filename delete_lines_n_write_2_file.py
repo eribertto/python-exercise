8cf7f33eda4caad716db8e7665a1859686a86dbc
erimendz_fed36_cinammon <erimendz@gmail.com>
2022-09-23 21:59:32 +0300

    change filename n rearrange lines

--- delete_lines_n_write_2_file.py
+++ delete_lines_n_write_2_file.py
@@ -5,14 +5,10 @@
 # NOTE: THIS IS A DESTRUCTIVE SCRIPT, IT DELETES LINES WITHOUT CONFIRMATION SO BEWARE
 
 # define the target file, must be absolute
-filename: str = "/home/erimendz/temp/read_demo.txt"
+filename: str = "/home/erimendz/temp/demo.file.txt"
 
 # read file
 with open(filename) as fp:
     lines = fp.readlines()
-
-# write to file excluding the deleted lines
-with open(filename, 'w') as fp:
-    # iterate each line
     for idx, line in enumerate(lines):
         print(f'{idx} {line}')
