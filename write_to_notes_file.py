58b1e4185b03bf4c5173eaad247ee2d26e637d6d
erimendz_fed36_cinammon <erimendz@gmail.com>
2022-09-23 14:24:57 +0300

    coded add_todo func balance del_todo

--- write_to_notes_file.py
+++ write_to_notes_file.py
@@ -5,27 +5,22 @@
 
 # define the target file, must be absolute
 filename: str = "/home/erimendz/note.txt"
+#note_to_write = input("\nWhat todo task to add? ")
 
-
-
-# add if name = main prelude
-## read the whole file first
-#with open(filename) as f:
-#    print(f.read().strip("\n"))
-
-
-# function add_todo
-def add_todo():
+def check_empty_input():
     note_to_write = input("\nWhat todo task to add? ")
     if len(note_to_write) == 0:
         print("None given, try again next time :-) ")
         exit()
-    # confirm to proceed
-    confirm_msg = "Proceed to write [N/y]?"
+    
+# function add_todo
+def add_todo():
+    #note_to_write = input("\nWhat todo task to add? ")
+    print(("You selected Add"))
+    check_empty_input()
+    confirm_msg = "Proceed [N/y]?"
     ask_confirm = input(confirm_msg)
-    # if ['y', 'ye', 'yes', 'YES'] in ask_confirm:    # lol this is written backwards
-    if ask_confirm in ["y", "ye", "yes", "YES"]:
-        # if yes, make upper case and write, else discard silently
+    if ask_confirm in ["y", "Y"]:
         note_to_write = note_to_write.upper()
         with open(filename, "a") as f:
             f.write("\n===||===\n")  # make this a function
@@ -38,9 +33,20 @@ def get_choice():
     if choice in ["a", "A"]:
         add_todo()
     elif choice in ["d", "D"]:
-        print("Do delete function")
+        del_todo()
+
+# function delete_todo
+def del_todo():
+    print(("You selected Delete"))
+    line_to_delete = input("What todo task to delete? ")
+    with open(filename) as f:
+       for line in f:
+           if line_to_delete.lower()  in line.lower():
+               pass
+
 
 get_choice()
+
 """
 feature enhancements:
 1. make this executable to be available in user environment
@@ -56,4 +62,8 @@ NB: for tabular format aka to use free space of desket
 consider using bash column command
 or is there an equivalent in python for tabular style write formatting
 
+# add if name = main prelude
+## read the whole file first
+#with open(filename) as f:
+#    print(f.read().strip("\n"))
 """
