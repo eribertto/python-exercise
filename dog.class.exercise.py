d07cf4d9934e8f08eeb58630e12ac8ddb508d1ee
erimendz_fed36_cinammon <erimendz@gmail.com>
2022-10-15 05:49:48 +0300

    create variables and class instances

--- dog.class.exercise.py
+++ dog.class.exercise.py
@@ -3,21 +3,24 @@
 # from book Python Class Course 2ed by Eric Matthes
 # page 158 dog.py
 
+
 class Dog:
-    """ A simple attempt to model a dog """
-    
+    """A simple attempt to model a dog"""
+
     def __init__(self, name, age):
-        """ Initialize name and age attributes """
+        """Initialize name and age attributes"""
         self.name = name
         self.age = age
-        
+
     def sit(self):
-        """ Simulate a dog sitting in response to a command
-        """
+        """Simulate a dog sitting in response to a command"""
         print(f"{self.name } is now sitting.")
 
     def roll_over(self):
-        """Simulate rolling over in response to a command
-        """
+        """Simulate rolling over in response to a command"""
         print(f"{self.name} rolled over!")
-        
+
+
+my_dog = Dog("Willie", 6)
+print(f"My dog's name is {my_dog.name}.")
+print(f"My dog's is {my_dog.age} years old.")
