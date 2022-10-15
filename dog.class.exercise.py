168705d03234dfed195dc842d26eea87865ca3c3
erimendz_fed36_cinammon <erimendz@gmail.com>
2022-10-15 05:41:31 +0300

    add formatting

--- dog.class.exercise.py
+++ dog.class.exercise.py
@@ -3,19 +3,21 @@
 # from book Python Class Course 2ed by Eric Matthes
 # page 158 dog.py
 
-
 class Dog:
-    """A simple attempt to model a dog"""
-
+    """ A simple attempt to model a dog """
+    
     def __init__(self, name, age):
-        """Initialize name and age attributes"""
+        """ Initialize name and age attributes """
         self.name = name
         self.age = age
-
+        
     def sit(self):
-        """Simulate a dog sitting in response to a command"""
+        """ Simulate a dog sitting in response to a command
+        """
         print(f"{self.name } is now sitting.")
 
     def roll_over(self):
-        """Simulate rolling over in response to a command"""
+        """Simulate rolling over in response to a command
+        """
         print(f"{self.name} rolled over!")
+        
