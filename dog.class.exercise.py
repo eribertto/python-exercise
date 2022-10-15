30d3662f112070d42ca72e8da3171f170094da94
erimendz_fed36_cinammon <erimendz@gmail.com>
2022-10-15 19:20:01 +0300

    add method to Dog lucy

--- dog.class.exercise.py
+++ dog.class.exercise.py
@@ -23,11 +23,12 @@ class Dog:
 
 my_dog = Dog("Willie", 6)
 print(f"My dog's name is {my_dog.name}.")
-print(f"My dog's is {my_dog.age} years old.")
+print(f"My dog is {my_dog.age} years old.")
 my_dog.sit()
 my_dog.roll_over()
 
 your_dog = Dog("Lucy", 3)
 print(f"\nYour dog's name is {your_dog.name}.")
-print(f"Your dog's is {your_dog.age} years old.")
+print(f"Your dog is {your_dog.age} years old.")
 your_dog.sit()
+your_dog.roll_over()
