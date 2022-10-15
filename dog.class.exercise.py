433ef268a8636cafa4fef4553c63f182517b3498
erimendz_fed36_cinammon <erimendz@gmail.com>
2022-10-15 06:02:42 +0300

    add new class instance

--- dog.class.exercise.py
+++ dog.class.exercise.py
@@ -24,3 +24,10 @@ class Dog:
 my_dog = Dog("Willie", 6)
 print(f"My dog's name is {my_dog.name}.")
 print(f"My dog's is {my_dog.age} years old.")
+my_dog.sit()
+my_dog.roll_over()
+
+your_dog = Dog("Lucy", 3)
+print(f"\nYour dog's name is {your_dog.name}.")
+print(f"Your dog's is {your_dog.age} years old.")
+your_dog.sit()
