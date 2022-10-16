73d5f22acce106e3255f89f70119665de04feee5
eribertto_tpad_endevour <erimendz@gmail.com>
2022-10-16 17:20:22 +0300

    add car class exercise

---
+++ car.class.exercise.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+# from book Python Class Course 2ed by Eric Matthes
+# about car Class
+
+
+class Car:
+    """A simple try to create a car Class"""
+
+    def __init__(self, make, model, year):
+        """Initialize attributes of the car Class"""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name"""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometer(self):
+        """Print a statement of the car's mileage"""
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, mileage):
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attempts to roll the odometer back.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You cannot roll back the odometer!")
+
+    def increment_odometer(self, miles):
+        """Add the given amount to the odo reading"""
+        self.odometer_reading += miles
+
+
+my_used_car = Car("subaru", "outback", 2020)
+print(my_used_car.get_descriptive_name())
