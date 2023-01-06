33f40daa8a1fca080fd2de829c1ccaef517ae257
erimendz_fed36_cinammon <erimendz@gmail.com>
2023-01-06 05:52:01 +0300

    add ranges

--- discounted_price.py
+++ discounted_price.py
@@ -11,9 +11,27 @@ If the price is less than 100, there will be a 5% discount.
 If the price is negative, there will be no discount.
 '''
 
+# create a range
+bracket20 = list(range(200,301))
+bracket10 = list(range(100,200))
+bracket5 = list(range(1,100))
+
 price = 250
 if price > 300:
+    print("30% discount!")
     discounted_price = price * (1-0.30)
     print(discounted_price)
+elif price in bracket20:
+    print("20% discount!")
+    discounted_price = price * (1-0.20)
+    print(discounted_price)
+elif price in bracket10:
+    print("10% discount")
+    discounted_price = price * (1-0.10)
+    print(discounted_price)
+elif price in bracket5:
+    print("5% discount")
+    discounted_price = price * (1-0.05)
+    print(discounted_price)
 else:
-    print("No discount for items less than 300")
+    print("No discount! ")
