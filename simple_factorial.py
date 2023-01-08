cbf37a08dcf56567e4f2a4c7890478bae39a015d
eribertto_arcoLinux <erimendz@gmail.com>
2023-01-08 17:20:19 +0300

    add looping through a list

--- simple_factorial.py
+++ simple_factorial.py
@@ -9,7 +9,7 @@ def factorial(number):
         # base case
         return 1
     if number < 0:
-     return -1
+        return -1
     # recursive case
     return number * factorial(number - 1)
 
@@ -25,3 +25,11 @@ for i in range(1, 11):
         print(f"{i} is even")
     else:
         print(f"{i} is odd")
+print("looping old list")
+float_list = [2.5, 16.42, 10.77, 8.3, 34.21]
+print(float_list)
+for i in range(0, len(float_list)):
+    float_list[i] = float_list[i] * 2  # double the item element
+# print the new mutable list
+print("looping new list")
+print(float_list)
