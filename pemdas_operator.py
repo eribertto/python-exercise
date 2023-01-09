6317481ed60a56b04a90170307f2330dcb693d73
eribertto_archlinux <erimendz@gmail.com>
2023-01-09 04:56:14 +0300

    add pemdas script

---
+++ pemdas_operator.py
@@ -0,0 +1,5 @@
+# PEMDASLeR math operator precedence in Python
+# Parens, Exponent, Multiply, Division, Add, Subtract and operation from Left to Right
+# note division yields a floating point number result
+msg = "The arithmetic result is: "
+print(f'{msg}', 3*3+3/3-3)
