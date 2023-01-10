86456fd253a49b1febaeb77a1379d1eec61fe686
erimendz_fed36_cinammon <erimendz@gmail.com>
2023-01-10 04:32:40 +0300

    add multi line comment

--- pemdas_operator.py
+++ pemdas_operator.py
@@ -3,3 +3,10 @@
 # note division yields a floating point number result
 msg = "The arithmetic result is: "
 print(f'{msg}', 3*3+3/3-3)
+
+'''order of precedence as follows
+multiply 3*3
+division 3/3 note this gives you float
+addition: multiply result + div result
+subtraction: add result subtracted by 3
+'''
