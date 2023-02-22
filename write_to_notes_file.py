8330da642ce20386a8acb6ac7a20d85dd0abd45e
erimendz_fed36_cinammon <erimendz@gmail.com>
2023-02-22 20:37:35 +0300

    make add todo function

--- write_to_notes_file.py
+++ write_to_notes_file.py
@@ -3,78 +3,61 @@
 # this is my small script to ask user for todo note to input & append to the note.txt file
 # so it will get appended IRT in the notes desklet of cinnamon DE
 
-# NOTE: THIS IS A DESTRUCTIVE SCRIPT, IT DELETES LINES WITHOUT CONFIRMATION SO BEWARE
-
 # define the target file, must be absolute
-filename: str = "/home/erimendz/note.txt"
-#note_to_write = input("\nWhat todo task to add? ")
-confirm_msg = "Proceed [N/y]?"
+filename: str = "/home/erimendz/temp/note.txt"
 
-def check_empty_input():
+# read the whole file first
+print("This is the file content (ignore the bars)")
+print("#" * 50)
+with open(filename) as f:
+    print(f.read().strip('\n'))
+print("#" * 50)
+
+# Add todo function
+def add_todo():
     note_to_write = input("\nWhat todo task to add? ")
     if len(note_to_write) == 0:
         print("None given, try again next time :-) ")
         exit()
-    
-# function add_todo
-def add_todo():
-    #note_to_write = input("\nWhat todo task to add? ")
-    print(("You selected Add"))
-    check_empty_input()
+    # confirm to proceed
+    print(f"You typed: {note_to_write}")
+    confirm_msg = "Proceed to write [N/y]?"
     ask_confirm = input(confirm_msg)
-    if ask_confirm in ["y", "Y"]:
+    # if ['y', 'ye', 'yes', 'YES'] in ask_confirm:    # lol this is written backwards
+    if ask_confirm in ['y', 'ye', 'yes', 'YES']:
+        # if yes, make upper case and write, else discard silently
         note_to_write = note_to_write.upper()
-        with open(filename, "a") as f:
-            f.write("\n===||===\n")  # make this a function
+        with open(filename, 'a') as f:
+            f.write("===||===\n")   # make this a function
             f.write(note_to_write)
+            print("Write action done!")
     else:
-        print("Bye!")
+        print("OK nothing is written, bye!")
 
+# get the users choice
 def get_choice():
     choice = input("[A]dd or [D]elete todo? ")
-    if choice in ["a", "A"]:
+    if choice in ['a', 'A']:
+        # call Add function
+        # print('Do add function!')
         add_todo()
-    elif choice in ["d", "D"]:
-        del_todo()
-
-# function delete_todo
-def del_todo():
-    print(("You selected Delete"))
-    line_to_delete = input("What todo task to delete? ")
-    breakpoint()
-    with open(filename) as f:
-        lines = f.readlines()       # this is a list
-    with open(filename, "w") as f:
-        for line in lines:
-            if line.strip("\n").lower() != line_to_delete.lower():
-                f.write(line)
-                
-        #print(type(lines))
-#       for line in f:
-#           if line_to_delete.lower()  in line.lower():
-#               print("This line will be deleted: ", line)
-#               line.
-#               print("Done")
+    elif choice in ['d', 'D']:
+        print("This feature is a todo, in progress.")
+    else:
+        print("Input is invalid, exiting!")
+        exit()
 
 get_choice()
 
-"""
-feature enhancements:
+
+'''
+TODO feature enhancements:
 1. make this executable to be available in user environment
-2. ask user if to add or delete todos in existing file
-3. create add and delete functions
+2. done: ask user if to add or delete todos in existing file
+3. done: create add and delete functions
 4. for delete function ask what line/lines to delete (you can visually see in the applet)
     tip: use module re
-5. for add function use the above
+5. done: for add function use the above
 6. save working file in python-exercise repo
-7. add name = main prelude
 
-NB: for tabular format aka to use free space of desket
-consider using bash column command
-or is there an equivalent in python for tabular style write formatting
-
-# add if name = main prelude
-## read the whole file first
-#with open(filename) as f:
-#    print(f.read().strip("\n"))
-"""
+'''
