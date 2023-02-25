8a7592a7d7a977662a4345dddb5f293baa5ac1d3
eribertto_arcoLinux <erimendz@gmail.com>
2023-02-25 15:43:43 +0300

    add check to url entries

--- youTube.downloader.py
+++ youTube.downloader.py
@@ -1,9 +1,16 @@
 # this script is for downloading youtube videos
 # https://www.freecodecamp.org/news/python-automation-scripts/
 
+import sys
 import pytube
 
 get_link = input("Enter the Youtube url: ")
+if not get_link:
+    print("Invalid url detected, exiting.... ")
+    sys.exit()
+if not get_link.startswith("https:") or not get_link.startswith("http:"):
+    print("Input is not a valid url: ")
+    sys.exit()
 video_download = pytube.YouTube(get_link)
 video_download.streams.first().download()
 print(f"Video is downloaded: {get_link}")
